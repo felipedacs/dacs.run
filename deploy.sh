e7069f6b37dd90bf3449f2beef1ee277205275a4
Felipe Silva <felipe.dacs@gmail.com>
2018-10-25 15:16:23 -0300

    deploy gl :fox_face:

--- deploy.sh
+++ deploy.sh
@@ -4,30 +4,53 @@ set -e
 # build
 npm run build
 
-# move CNAME
-cp CNAME dist/CNAME
-
 # navigate into the build output directory
 cd dist
 
-
-
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'dacs.run' > CNAME
+echo 'https://github.com/felipedacs/dacs.run' > readme.md
 
 git init
 git add -A
-git commit -m 'deploy'
-
-
-# branch gh-pages
-#git checkout -b master
+git commit -m 'deploy gh :octocat:'
 
 # if you are deploying to https://<USERNAME>.github.io
 git push -f https://github.com/felipedacs/felipedacs.github.io.git master
+
+
+# mkdir public
+cd ..
+mv dist gitlab-deploy
+mkdir dist
+mv gitlab-deploy dist/public
+# move arquivo gitlab
+cp .gitlab-ci.yml dist/.gitlab-ci.yml
+cd dist
+git add -A
+git commit -m 'deploy gl :fox_face:'
 git push -f https://gitlab.com/felipedacs/felipedacs.gitlab.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+cd ..
 cd -
+
+
+
+
+
+
+
+
+
+# # mkdir public
+# cd ..
+# mkdir gitlab
+# cp -r dist/* gitlab
+# mv gitlab dist/public
+# # move arquivo gitlab
+# cp .gitlab-ci.yml dist/.gitlab-ci.yml
+# cd dist
+# git add -A
+# git commit -m 'deploy gl :fox_face:'
+# git push -f https://gitlab.com/felipedacs/felipedacs.gitlab.io.git master
+# cd ..
+# cd -
